b3879357a8cd5ce014fc249ea931c98d95952295
Jorge Murta <jorge@jorgemurta.com>
2022-03-19 11:08:52 +0000

    chore: change a bit the UI

--- resources/js/Pages/ShortUrl/Index.vue
+++ resources/js/Pages/ShortUrl/Index.vue
@@ -9,10 +9,13 @@
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <form @submit.prevent="addUrl" class="bg-white overflow-hidden flex space-x-4 p-4 rounded-lg">
-                    <input v-model="addUrlForm.redirect_to" type="text" placeholder="Enter here the url you want to short: ex. https://google.com/" class="flex-1 rounded-lg border-gray-200">
-                    <input v-model="addUrlForm.slug" type="text" placeholder="Want a custom slug?" class="w-64 rounded-lg border-gray-200">
-                    <button type="submit" class="bg-slate-800 hover:bg-slate-900 text-white font-medium px-6 rounded-lg">Make it shorter</button>
+                <h1 class="text-xl font-semibold pb-4">Let's make it shorter</h1>
+                <form @submit.prevent="addUrl" class="bg-white overflow-hidden p-4 rounded-lg">
+                    <div class="flex space-x-4">
+                        <input v-model="addUrlForm.redirect_to" type="text" placeholder="Enter here the url you want to short: ex. https://google.com/" class="flex-1 rounded-lg border-gray-200">
+                        <input v-model="addUrlForm.slug" type="text" placeholder="Want a custom slug?" class="w-64 rounded-lg border-gray-200">
+                        <button type="submit" class="bg-slate-800 hover:bg-slate-900 text-white font-medium px-6 rounded-lg">Make it shorter</button>
+                    </div>
                 </form>
             </div>
         </div>
@@ -27,15 +30,17 @@
                     Nothing to show in here
                 </div>
                 <div v-else class="grid grid-cols-3 gap-4">
-                    <div v-for="url in urls" :key="url.id" class="border rounded-lg p-6 bg-white">
+                    <div v-for="url in urls" :key="url.id" class="rounded-lg p-6 bg-white flex flex-col justify-between">
                         <div class="flex flex-col items-center pb-2">
                             <a :href="url.url" class="hover:text-blue-500 font-medium hover:underline">{{url.url}}</a>
                             <div class="py-2 text-gray-600 text-sm">redirects to</div>
                             <a :href="url.redirect_to" class="hover:text-blue-500 hover:underline break-all">{{ url.redirect_to }}</a>
                         </div>
-                        <div class="flex justify-between text-xs border-t -mx-6 pt-4 -mb-2 px-6 mt-6">
+                        <div class="flex justify-between text-xs border-t border-gray-200 -mx-6 pt-4 -mb-2 px-6 mt-6">
                             <div><button>Copy Url</button></div>
-                            <div>{{ url.visit_count }} time(s) clicked</div>
+                            <div v-if="url.visit_count === 0">Never used</div>
+                            <div v-else-if="url.visit_count === 1">Used {{ url.visit_count }} time</div>
+                            <div v-else>Used {{ url.visit_count }} times</div>
                         </div>
                     </div>
                 </div>
